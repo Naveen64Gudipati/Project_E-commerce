620909e4aed7397a5d3349cefe3e2fa4335d2e94
Naveen Kumar Gudipati <gudipatinaveenkumar6300@gmail.com>
2023-09-22 08:10:17 +0530

    project

---
+++ Ecommerce-project/JS/Page-JS/index.js
@@ -0,0 +1,10 @@
+
+// Menu bar
+function menu(){
+   document.getElementById("frame1").classList.toggle("style");
+}
+
+// Add to cart
+function opencart(){
+    document.getElementById("cartmenu").classList.toggle("addtocart")
+}
